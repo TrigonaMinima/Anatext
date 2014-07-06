5da1522bcdaed1d8b2e11fc1ce00cdaada0a85bf
TrigonaMinima <shivamrana95@yahoo.in>
2014-07-07 00:46:30 +0530

    gui started with pyqt4

---
+++ GUI/proro1.py
@@ -0,0 +1,86 @@
+import sys
+from PyQt4 import QtGui, QtCore
+
+
+class Example(QtGui.QMainWindow):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+
+        # self.text = 'AnaText'
+        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)        
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtGui.qApp.quit)
+
+        openFile = QtGui.QAction(QtGui.QIcon('open.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showDialog)
+
+        about = QtGui.QAction(QtGui.QIcon('about.png'), 'About', self)
+        # about.setShortcut('Ctrl+O')
+        about.setStatusTip('About AnaText')
+        about.triggered.connect(self.paintEvent)
+
+        documentation = QtGui.QAction(QtGui.QIcon('doc.png'), 'Documentation', self)
+        # documentation.setShortcut('Ctrl+O')
+        documentation.setStatusTip('Documentation')
+        # documentation.triggered.connect(self.showDialog)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+        fileMenu.addAction(exitAction)
+
+        fileMenu = menubar.addMenu('&View')
+
+        fileMenu = menubar.addMenu('&Tools')
+
+        fileMenu = menubar.addMenu('&Help')
+        fileMenu.addAction(documentation)
+        fileMenu.addAction(about)
+
+        self.resize(700, 400)
+        self.center()
+        
+        self.setWindowTitle('AnaText v1.0')
+        self.show()
+        
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes | 
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def showDialog(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+                '/home')
+
+
+
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ GUI/test_ui.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'test.ui'
+#
+# Created: Mon Jul  7 12:20:15 2014
+#      by: PyQt4 UI code generator 4.10
+#
+# WARNING! All changes made in this file will be lost!
+
